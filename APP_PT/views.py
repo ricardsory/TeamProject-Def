78849ec6f51e99a281a13a1c6ad4703b0284cd1f
Ricard Sory <ricardsory@gmail.com>
2016-02-28 17:29:08 +0100

    THIRD COMMIT

--- APP_PT/views.py
+++ APP_PT/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import User
 from .forms import CreateAccount
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -9,5 +10,11 @@ def login(request):
     return render(request, 'APP_PT/login.html', {'users': users})
 
 def newUser(request):
-    form = CreateAccount()
-    return render(request, 'APP_PT/newUser.html', {'form': form})
+    if request.method == "POST":
+        form = CreateAccount(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('APP_PT.views.login')
+    else:
+        form = CreateAccount()
+        return render(request, 'APP_PT/newUser.html', {'form': form})
