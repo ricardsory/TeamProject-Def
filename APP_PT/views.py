4b5a88e86172f9b6020ef8f95f746155bffabad7
Ricard Sory <ricardsory@gmail.com>
2016-02-28 16:43:30 +0100

    SECOND COMMIT

--- APP_PT/views.py
+++ APP_PT/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+
+def login(request):
+    users = User.objects.all()
+    return render(request, 'APP_PT/login.html', {'users': users})
